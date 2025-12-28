32a97638ce1f6c524dde7a23474cc9f18d93bc40
Edson Carreon <edsonjaybcarreon@gmail.com>
2025-12-28 17:38:11 +0800

    cleanup exceptions

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,25 +1,17 @@
-"""Custom exceptions for Epic Games API client."""
+"""Epic Games API exceptions."""
 
 
 class EpicGamesAPIError(Exception):
-    """Base exception for Epic Games API errors."""
-
     pass
 
 
 class NetworkError(EpicGamesAPIError):
-    """Raised when network request fails."""
-
     pass
 
 
 class InvalidResponseError(EpicGamesAPIError):
-    """Raised when API response is invalid or cannot be parsed."""
-
     pass
 
 
 class RateLimitError(EpicGamesAPIError):
-    """Raised when API rate limit is exceeded."""
-
     pass
